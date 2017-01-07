e4b06d76be00d6e429362df24dd42442359e69e6
Phil Cote <cotejrp@gmail.com>
2017-01-07 17:10:10 -0500

    License, info block, and minor pep8 cleanup.

--- grease_empties.py
+++ grease_empties.py
@@ -1,5 +1,41 @@
+# grease_empties.py Copyright (C) 2016, cotejrp1
+#
+# Relaxes selected vertices while retaining the shape as much as possible
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+bl_info = {
+    "name": "Grease Empties",
+    "author": "cotejrp1",
+    "version": (1, 0),
+    "blender": (2, 75, 0),
+    "location": "To be determined",
+    "description": "Generates a set of empty objects that follow a ",
+    "warning": "work in progress",
+    "wiki_url": "none",
+    "category": "Uncategorized",
+}
+
 import bpy
 from bpy.props import IntProperty, EnumProperty
+
 
 def points(strokes):
     for stroke in strokes:
